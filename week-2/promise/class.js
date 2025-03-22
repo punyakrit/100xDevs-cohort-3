4a0a588b1b5bc663a9c46712901dc6cb19163e75
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-03-23 02:36:04 +0530

    promise

--- week-2/promise/class.js
+++ week-2/promise/class.js
@@ -1,21 +1,44 @@
-class Rectangle {
-  constructor(width, height, color) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-  }
+// class Rectangle {
+//   constructor(width, height, color) {
+//     this.width = width;
+//     this.height = height;
+//     this.color = color;
+//   }
 
-  area() {
-    const area = this.width * this.height;
-    return area;
-  }
+//   area() {
+//     const area = this.width * this.height;
+//     return area;
+//   }
 
-  paint() {
-    console.log(`Painting with color ${this.color}`);
-  }
+//   paint() {
+//     console.log(`Painting with color ${this.color}`);
+//   }
+// }
+
+// const rect = new Rectangle(2, 4);
+// const area = rect.area();
+// console.log(area);
+// rect.paint()
+
+// const date = new Date()
+
+// console.log(date)
+
+// function setTimeoutPromisified(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// function callback() {
+// 	console.log("3 seconds have passed");
+// }
+
+// setTimeoutPromisified(3000).then(callback)
+
+function random(resolve){
+resolve()
 }
-
-const rect = new Rectangle(2, 4);
-const area = rect.area();
-console.log(area);
-rect.paint()
+function abc(){
+  console.log("hello")
+}
+let p = new Promise(random)
+p.then(abc)
