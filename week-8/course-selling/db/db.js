0c0330a9ccba0f53e752fb317aa792174107962a
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-04-06 19:23:49 +0530

    db schema

--- week-8/course-selling/db/db.js
+++ week-8/course-selling/db/db.js
@@ -1,5 +1,52 @@
 const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://admin:wyWWjD7kVASGlKJh@cluster0.gt81chd.mongodb.net/week-8-course-Selling')
 
-const schema = mongoose.Schema()
+const schema = mongoose.Schema
 
-// const uID = schema.
+
+const userSchema = new schema({
+    email : {type : String , unique : true},
+    password: String,
+    name : String
+})
+
+const adminSchema = new schema({
+    email : {type : String , unique : true},
+    password: String,
+    name : String
+})
+
+const courseSchema = new schema({
+    title : String,
+    description: String,
+    price : Number,
+    imageUrl : String,
+    creatorId : {
+        type: mongoose.Schema.ObjectId,
+        ref : "Admin"
+    }
+})
+
+const purchaseSchema = new schema({
+    courseId: {
+        type: mongoose.Schema.ObjectId,
+        ref : "Course"
+    },
+    userId : {
+        type: mongoose.Schema.ObjectId,
+        ref : "User"
+    }
+})
+
+const userModel = mongoose.model('User' , userSchema)
+const adminModel = mongoose.model('Admin' ,adminSchema )
+const courseModel = mongoose.model('Course' , courseSchema)
+const purchaseModel = mongoose.model('Purchase' , purchaseSchema)
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
