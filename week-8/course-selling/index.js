e36b44f232e90c9cc182af3e9533a512715a4325
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-04-06 18:50:13 +0530

    admin route

--- week-8/course-selling/index.js
+++ week-8/course-selling/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { userRouter} = require('./routes/userAuth')
 const { courseRouter } = require('./routes/courses')
+const { adminRouter} = require('./routes/adminRoute')
 const app = express()
 
 
 app.use('/user', userRouter)
+app.use('/admin', adminRouter)
 app.use('/course' , courseRouter)
 
 app.listen(3000)
+
---
+++ week-8/course-selling/routes/adminRoute.js
@@ -0,0 +1,28 @@
+const express= require('express')
+
+const adminRouter = express.Router()
+
+adminRouter.post('/signup', (req,res)=>{
+
+})
+
+adminRouter.post('/signin', (req,res)=>{
+    
+})
+
+adminRouter.post('/course',(req,res)=>{
+
+})
+
+adminRouter.put('/course',(req,res)=>{
+    
+})
+
+adminRouter.get('/course/bulk',(req,res)=>{
+    
+})
+
+
+module.exports = {
+    adminRouter
+}
