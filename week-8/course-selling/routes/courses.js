ae953728b10e5f488db9dc07df6f92394c341e37
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-04-07 11:50:17 +0530

    course ROute

--- week-8/course-selling/routes/courses.js
+++ week-8/course-selling/routes/courses.js
@@ -1,15 +1,25 @@
 const express = require('express')
-
+const {userAuth} = require('../middleware/user')
+const { userModel, courseModel, purchaseModel } = require('../db/db')
 const courseRouter = express.Router()
 
-courseRouter.get('/all',(req,res)=>{
+courseRouter.get('/all',userAuth ,async (req,res)=>{
+    const allCourse = await courseModel.find({})
     res.json({
-        courses : []
+        allCourse
     })
 })
 
-courseRouter.post('/purchase',(req,res)=>{
-    
+courseRouter.post('/purchase',userAuth, async(req,res)=>{
+    const {courseId} = req.body
+    const userId = req.userId
+    const insertData = await purchaseModel.create({
+        courseId : courseId,
+        userId : userId
+    })
+    res.json({
+        message:"Course Bought"
+    })
 })
 
 module.exports ={
