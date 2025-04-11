57a77e3aae482ed1a5bd8ce00fe0c9b7702538ca
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-04-11 18:20:05 +0530

    ref clock

--- week10-react/routing/src/App.jsx
+++ week10-react/routing/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const rer = useRef()
+
+  function StartClock() {
+    const value = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    rer.value = value
+  }
+
+  function stop() {
+    clearInterval(rer.value);
+  }
+
   return (
     <div>
-      dws
+      {count}
+      <div>
+        <button onClick={StartClock}>Start</button>
+        <button onClick={stop}>Stop</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
