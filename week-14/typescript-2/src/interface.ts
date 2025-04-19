a5717adf5d04b67679716cf07f84eb172a451afa
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-04-19 16:46:42 +0530

    types

--- week-14/typescript-2/src/interface.ts
+++ week-14/typescript-2/src/interface.ts
@@ -1,21 +1,48 @@
 
-interface Address {
-    city: string;
-    pincode?: number
-};
+// interface Address {
+//     city: string;
+//     pincode?: number
+// };
 
-interface User {
-    firstName: string;
-    lastName: string;
-    age: number;
-    address: Address
+// interface User {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     address: Address
+// }
+
+// const user: User = {
+//     firstName:"Anoop",
+//     lastName:"Singh",
+//     age :23,
+//     address:{
+//         city:"ASR"
+//     }
+// }
+
+interface Person{
+    name : string
+    age: number
+    // greet : ()=> string
 }
 
-const user: User = {
-    firstName:"Anoop",
-    lastName:"Singh",
-    age :23,
-    address:{
-        city:"ASR"
+const person : Person = {
+    name: "Anoop",
+    age:22,
+    // greet() {
+    //     return "Hi"
+    // },
+}
+
+class Manager implements Person{
+    name: string;
+    age: number;
+
+    constructor(name: string, age:number){
+        this.name = name
+        this.age = age
     }
 }
+
+let user = new Manager("abc", 21)
+console.log(user.age)
---
+++ week-14/typescript-2/src/type.ts
@@ -0,0 +1,13 @@
+// interface User {
+//     name:string,
+//     age:number
+// }
+
+type User = {
+    name : string,
+    age : number
+}
+
+function isLeage(user :User){
+    return user.age
+}
