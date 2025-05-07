ee789cec71b7a943d9231333984d3300875b3f37
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-05-07 23:47:19 +0530

    chat - fe

--- week-18.2/backend/src/index.ts
+++ week-18.2/backend/src/index.ts
@@ -3,8 +3,16 @@ import {WebSocketServer} from 'ws'
 const wss = new WebSocketServer({port:8080 })
 
 let user = 0
+let allSocket = []
 
 wss.on('connection', (socket)=>{
+    allSocket.push(socket)
     user++;
     socket.send("uo" + user)
+
+    socket.on("message",(message)=>{
+        allSocket.forEach((s)=>{
+            s.send(message.toString() + " form user ")
+        })
+    })
 })
