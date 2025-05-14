11ee66b56dcad1196c1e448f5ba05ea28bd56c35
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-05-14 18:02:02 +0530

    implementation

--- react-tanstack/src/App.jsx
+++ react-tanstack/src/App.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-function App() {
-  const [values, setValues] = useState([]);
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
-  useEffect(() => {
-    async function call() {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts/"
-      );
-      setValues(response.data);
-    }
-    call();
-  }, []);
+const queryClient = new QueryClient();
+
+async function getter() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts/"
+  );
+  return response.data;
+}
+
+function App() {
   return (
     <div>
-      {values.map((data) => {
-        return <div key={data.id}>{data.title}</div>;
-      })}
+      <QueryClientProvider client={queryClient}>
+        <Posts />
+      </QueryClientProvider>
     </div>
   );
+}
+
+function Posts() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getter,
+    refetchInterval: 4*1000
+  });
+
+  if (error) {
+    return <div>Error while fetching</div>;
+  }
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
