3afb42ec5bef8f1da925c162041fd9441c294a95
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-05-17 17:03:34 +0530

    prisma connection-save

--- next-backend/app/api/v1/signup/route.ts
+++ next-backend/app/api/v1/signup/route.ts
@@ -1,10 +1,21 @@
-import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const client = new PrismaClient()
 
 export async function POST(req: NextRequest){
     const body = await req.json()
-    
 
+    const respoonse = await client.user.create({
+        data:{
+            name: body.name,
+            password:body.password
+        }
+    })
 
+    return NextResponse.json({
+        message :"User Added"
+    })
 
 
 }
